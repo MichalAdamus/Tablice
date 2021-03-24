8db7ddd0b0564f07d72269b85e797d86812fee50
Michał Adamus <mb.adamus@gmail.com>
2021-03-24 10:08:44 +0100

    #1 Zadanie 1 z Karolem Michniewiczem

---
+++ OneDimensionArray.java
@@ -0,0 +1,38 @@
+class OneDimensionArray{
+	public static void main(String args[]){
+	// deklaracja i alokacja
+	char[] name = new char[5];
+	char[] surname = new char[11];
+	int age;
+	
+	// inicjalizacja
+	name[0] = 'K';
+	name[1] = 'a';
+	name[2] = 'r';
+	name[3] = 'o';
+	name[4] = 'l';
+
+	surname[0] = 'M';
+	surname[1] = 'i';
+	surname[2] = 'c';
+	surname[3] = 'h';
+	surname[4] = 'n';
+	surname[5] = 'i';
+	surname[6] = 'e';
+	surname[7] = 'w';
+	surname[8] = 'i';
+	surname[9] = 'c';
+	surname[10] = 'z';
+
+	age = 33;
+	
+	System.out.print(name[0]);
+	System.out.print(name[1]);
+	System.out.print(name[2]);
+	System.out.print(name[3]);
+	System.out.print(name[4]);
+	System.out.println();
+	
+	
+	}
+}
