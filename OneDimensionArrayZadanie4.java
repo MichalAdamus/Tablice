5ca0306d5c7bb7ad434fd0fecdcc36347e2b9faa
Michał Adamus <mb.adamus@gmail.com>
2021-03-24 12:50:30 +0100

    #4 zadanie do przechowywania ocen z matematyki

---
+++ OneDimensionArrayZadanie4.java
@@ -0,0 +1,20 @@
+class OneDimensionArray{
+    public static void main(String args[]){
+        // deklaracja i alokacja
+        double[] StudentGrades = {1.5,2.5,1,2,3};
+        
+        // wyświetlenie 3 ostatnich ocen
+        
+        System.out.println("-------------------------");
+
+        System.out.print("Oceny z Informatyki: ");
+        System.out.print(StudentGrades[3]);
+        System.out.print(StudentGrades[4]);
+        System.out.println();
+        System.out.println("-------------------------");
+		
+		// Poprawienie drugiej oceny -> wstawienie wartości "5"
+		
+				
+    }
+}
